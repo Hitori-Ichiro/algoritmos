bffa348f0c31a86c2d4cc007c40b4f00e7a44a78
Hitori_ichiro <angeljose160203@gmail.com>
2025-11-12 18:58:16 -0500

    v1

---
+++ ejercicio1.py
@@ -0,0 +1,6 @@
+#desarrollar un algoritmo que determine si un número es par o impar.
+num = int(input("Ingrese un número: "))
+if num % 2 == 0:
+    print(f"El número {num} es par.")
+else:
+    print(f"El número {num} es impar.")
